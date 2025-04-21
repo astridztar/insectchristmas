8c7134c406ccec9fc5151f03d2be542b22db748b
astrid ztar <158858196+astridztar@users.noreply.github.com>
2025-04-20 23:12:19 -0500

    js update 04/20/2025

--- js/nav.js
+++ js/nav.js
@@ -15,16 +15,17 @@ function replaceNavigationContent() {
                 <a href="https://insect.christmas/cosplay" target="_parent"><img src="https://insect.christmas/buttons/button_cosplay.png"></a>
                 
                 <br>
-                
+                <a href="https://insect.christmas/forum" target="_parent"><img src="https://insect.christmas/buttons/button_forum.png"></a>
                 <a href="https://insect.christmas/games" target="_parent"><img src="https://insect.christmas/buttons/button_games.png"></a>
                 <a href="https://insect.christmas/music" target="_parent"><img src="https://insect.christmas/buttons/button_music.png"></a>
-                <a href="https://insect.christmas/stream" target="_parent"><img src="https://insect.christmas/buttons/button_stream.png"></a>
+                
                 <br>
-                <a href="https://insect.christmas/thoughts" target="_parent"><img src="https://insect.christmas/buttons/button_thoughts.png"></a>
+                
+                 <a href="https://insect.christmas/thoughts" target="_parent"><img src="https://insect.christmas/buttons/button_thoughts.png"></a>
                 
                 <a href="https://insect.christmas/video" target="_parent"><img src="https://insect.christmas/buttons/button_video.png"></a>
                 
-                <a href="https://insect.christmas/other" target="_parent"><img src="https://insect.christmas/buttons/button_other.png"></a>
+                <a href="https://insect.christmas/dir" target="_parent"><img src="https://insect.christmas/buttons/button_other.png"></a>
                 
                 
                 <hr style="color:chartreuse;">
@@ -32,18 +33,22 @@ function replaceNavigationContent() {
                 
                 
                 <a href="https://discord.gg/tV6uBJYqC8" target="_parent"><img src="https://insect.christmas/buttons/button_discord.png"></a>
-                <a href="https://insect.christmas/external" target="_parent"><img src="https://insect.christmas/buttons/button_links.png"></a>
+                <a href="https://insect.christmas/links" target="_parent"><img src="https://insect.christmas/buttons/button_links.png"></a>
                 <a href="https://lilacboy.store" target="_parent"><img src="https://insect.christmas/buttons/button_merch.png"></a>
                 
                 <hr style="color:chartreuse;">
                 
-                <img src="https://insect.christmas/blinkies/debian.gif">
+                <img src="https://insect.christmas/blinkies/debian2.gif">
                 <img src="https://insect.christmas/blinkies/lesbian.gif"><br>
-                <img src="https://insect.christmas/blinkies/marxism.gif">
-                <img src="https://insect.christmas/blinkies/neurodivergent.gif">
                 
                 <hr style="color:chartreuse;">
-                <br>
+                
+                
+                <b>lifetime website hits:</b><br>
+                <div align='center'><a href='https://www.free-website-hit-counter.com'><img src='https://www.free-website-hit-counter.com/zc.php?d=9&id=5974&s=1' border='0' alt='Free Website Hit Counter'></a></div>
+                
+                <hr style="color:chartreuse;">
+                
                 <img class="kiwiPic" id="kiwi" src="" width="200" height="200">
                 <br>
                 <b id="kiwiText"></b>
@@ -57,9 +62,9 @@ function replaceNavigationContent() {
 }
 
 // kiwi images array
-var kiwiImages = ["https://insect.christmas/kiwi/kiwi1.png", "https://insect.christmas/kiwi/kiwi2.png", "https://insect.christmas/kiwi/kiwi3.png", "https://insect.christmas/kiwi/kiwi4.png", "https://insect.christmas/kiwi/kiwi5.png", "https://insect.christmas/kiwi/kiwi6.png", "https://insect.christmas/kiwi/kiwi7.png", "https://insect.christmas/kiwi/kiwi8.png", "https://insect.christmas/kiwi/kiwi9.png", "https://insect.christmas/kiwi/kiwi10.png", "https://insect.christmas/kiwi/kiwi11.png", "https://insect.christmas/kiwi/kiwi12.png", "https://insect.christmas/kiwi/kiwi13.png", "https://insect.christmas/kiwi/kiwi14.png", "https://insect.christmas/kiwi/kiwi15.png", "https://insect.christmas/kiwi/kiwi16.png", "https://insect.christmas/kiwi/kiwi17.png", "https://insect.christmas/kiwi/kiwi18.png", "https://insect.christmas/kiwi/kiwi19.png", "https://insect.christmas/kiwi/kiwi20.png", "https://insect.christmas/kiwi/kiwi21.png", "https://insect.christmas/kiwi/kiwi22.png", "https://insect.christmas/kiwi/kiwi23.png", "https://insect.christmas/kiwi/kiwi24.png", "https://insect.christmas/kiwi/kiwi25.png", "https://insect.christmas/kiwi/kiwi26.png", "https://insect.christmas/kiwi/kiwi27.png", "https://insect.christmas/kiwi/kiwi28.png", "https://insect.christmas/kiwi/kiwi29.png", "https://insect.christmas/kiwi/kiwi30.png", "https://insect.christmas/kiwi/kiwi31.png", "https://insect.christmas/kiwi/kiwi32.png", "https://insect.christmas/kiwi/kiwi33.png", "https://insect.christmas/kiwi/kiwi34.png", "https://insect.christmas/kiwi/kiwi35.png", "https://insect.christmas/kiwi/kiwi36.png", "https://insect.christmas/kiwi/kiwi37.png", "https://insect.christmas/kiwi/kiwi38.png", "https://insect.christmas/kiwi/kiwi39.png", "https://insect.christmas/kiwi/kiwi40.png", "https://insect.christmas/kiwi/kiwi41.png", "https://insect.christmas/kiwi/kiwi42.png"]
+var kiwiImages = ["https://insect.christmas/kiwi/kiwi1.png", "https://insect.christmas/kiwi/kiwi2.png", "https://insect.christmas/kiwi/kiwi3.png", "https://insect.christmas/kiwi/kiwi4.png", "https://insect.christmas/kiwi/kiwi5.png", "https://insect.christmas/kiwi/kiwi6.png", "https://insect.christmas/kiwi/kiwi7.png", "https://insect.christmas/kiwi/kiwi8.png", "https://insect.christmas/kiwi/kiwi9.png", "https://insect.christmas/kiwi/kiwi10.png", "https://insect.christmas/kiwi/kiwi11.png", "https://insect.christmas/kiwi/kiwi12.png", "https://insect.christmas/kiwi/kiwi13.png", "https://insect.christmas/kiwi/kiwi14.png", "https://insect.christmas/kiwi/kiwi15.png", "https://insect.christmas/kiwi/kiwi16.png", "https://insect.christmas/kiwi/kiwi17.png", "https://insect.christmas/kiwi/kiwi18.png", "https://insect.christmas/kiwi/kiwi19.png", "https://insect.christmas/kiwi/kiwi20.png", "https://insect.christmas/kiwi/kiwi21.png", "https://insect.christmas/kiwi/kiwi22.png", "https://insect.christmas/kiwi/kiwi23.png", "https://insect.christmas/kiwi/kiwi24.png", "https://insect.christmas/kiwi/kiwi25.png", "https://insect.christmas/kiwi/kiwi26.png", "https://insect.christmas/kiwi/kiwi27.png", "https://insect.christmas/kiwi/kiwi28.png", "https://insect.christmas/kiwi/kiwi29.png", "https://insect.christmas/kiwi/kiwi30.png", "https://insect.christmas/kiwi/kiwi31.png", "https://insect.christmas/kiwi/kiwi32.png", "https://insect.christmas/kiwi/kiwi33.png", "https://insect.christmas/kiwi/kiwi34.png", "https://insect.christmas/kiwi/kiwi35.png", "https://insect.christmas/kiwi/kiwi36.png", "https://insect.christmas/kiwi/kiwi37.png", "https://insect.christmas/kiwi/kiwi38.png", "https://insect.christmas/kiwi/kiwi39.png", "https://insect.christmas/kiwi/kiwi40.png", "https://insect.christmas/kiwi/kiwi41.png", "https://insect.christmas/kiwi/kiwi42.png", "https://insect.christmas/kiwi/kiwi43.png", "https://insect.christmas/kiwi/kiwi44.png", "https://insect.christmas/kiwi/kiwi45.png", "https://insect.christmas/kiwi/kiwi46.png", "https://insect.christmas/kiwi/kiwi47.png", "https://insect.christmas/kiwi/kiwi48.jpeg", "https://insect.christmas/kiwi/kiwi49.png", "https://insect.christmas/kiwi/kiwi50.png", "https://insect.christmas/kiwi/kiwi51.jpeg"]
 
-var kokoImages = ["https://insect.christmas/kiwi/coco1.png", "https://insect.christmas/kiwi/coco2.png","https://insect.christmas/kiwi/coco3.png","https://insect.christmas/kiwi/coco4.png","https://insect.christmas/kiwi/coco5.png","https://insect.christmas/kiwi/coco6.png","https://insect.christmas/kiwi/coco7.png","https://insect.christmas/kiwi/coco8.png","https://insect.christmas/kiwi/coco9.png","https://insect.christmas/kiwi/coco10.png","https://insect.christmas/kiwi/coco11.png","https://insect.christmas/kiwi/coco12.png","https://insect.christmas/kiwi/coco13.png","https://insect.christmas/kiwi/coco14.png",]
+var kokoImages = ["https://insect.christmas/kiwi/coco1.png", "https://insect.christmas/kiwi/coco2.png","https://insect.christmas/kiwi/coco3.png","https://insect.christmas/kiwi/coco4.png","https://insect.christmas/kiwi/coco5.png","https://insect.christmas/kiwi/coco6.png","https://insect.christmas/kiwi/coco7.png","https://insect.christmas/kiwi/coco8.png","https://insect.christmas/kiwi/coco9.png","https://insect.christmas/kiwi/coco10.png","https://insect.christmas/kiwi/coco11.png","https://insect.christmas/kiwi/coco12.png","https://insect.christmas/kiwi/coco13.png","https://insect.christmas/kiwi/coco14.png","https://insect.christmas/kiwi/coco15.png","https://insect.christmas/kiwi/coco16.png", "https://insect.christmas/kiwi/koko17.jpeg","https://insect.christmas/kiwi/koko18.png","https://insect.christmas/kiwi/koko19.jpeg"]
 
 function randomKiwi() {
     // decide kiwi or coco
