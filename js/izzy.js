d391fae8629cb5e4ded8383083dbcdd285f24c86
Matthew Roberts <114992138+hetbet@users.noreply.github.com>
2025-04-27 04:54:24 +0100

    refactor(fe): replace izzy if-else clutter
    Replaced if-else ladder with a structured rule set message system. Doing so allows better scalability, and increases readability and efficiency. plus silly.

--- js/izzy.js
+++ js/izzy.js
@@ -9,6 +9,54 @@ const izzyEmotions = [
     '^_^', '^o^', '^~^', "^v^", "^x^", "^D^", "^c^", "^.^", "^,^", "^u^", "^-^"
 ];
 
+const izzyClickRules = [
+    { type: "range", min: 5000, max: Infinity, message: "okay. you revived her. happpy?" },
+    { type: "range", min: 1000, max: 4999, message: "RECONSIDER EXISTING!!!!!!! F#CK!!!." },
+    { type: "range", min: 500, max: 999, message: "get a life, scumbag." },
+    { type: "range", min: 100, max: 499, message: "izzy has died. you moron. you f#cking killed her." },
+    { type: "range", min: 90, max: 99, message: "STOP!!!!!!!!!!!!!!!!!!!!!!" },
+    { type: "range", min: 60, max: 75, message: "oh god... her wings won't work anymore..." },
+    { type: "exact", value: 50, message: "dude, you're gonna squish her..." },
+    { type: "range", min: 10, max: 19, message: "izzy is bored of this. maybe you should give up?" },
+    { type: "range", min: 5, max: 9, message: "she starts gnawing on you..." } ,
+    { type: "range", min: 2, max: 4, message: "maybe she's getting used to you?" },
+    { type: "exact", value: 1, message: "izzy seems rather puzzled." }
+];
+
+function outputIzzyMessage(izzyClicks) {
+    // grab random emotion
+    var randomEmotion = izzyEmotions[Math.floor(Math.random() * izzyEmotions.length)];
+
+    // default izzy message
+    let izzyMessage = "stop clicking her! wtf!";
+
+    // default izzy emotion
+    let izzyEmotion = String(randomEmotion);
+
+    for (let i = 0; i < izzyClickRules.length; i++) {
+        const rule = izzyClickRules[i];
+
+        if (rule.type === "exact" && izzyClicks === rule.value) {
+            izzyMessage = rule.message;
+            break;
+
+        } else if (rule.type === "range" && izzyClicks >= rule.min && izzyClicks <= rule.max) {
+            // If between following ranges add hard coded faces
+            if (izzyClicks >= 100 && izzyClicks < 5000 ) {
+                izzyEmotion = String("X_X");
+            } else if (izzyClicks >= 5 && izzyClicks < 10 ) {
+                izzyEmotion = String("^W^");
+            }
+
+            izzyMessage = rule.message;
+            break;
+        }   
+    }
+
+    // Output message in alert
+    alert(izzyMessage + "\n\nizzy: " + izzyEmotion + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.");
+}
+
 izzy.addEventListener("click", function() {
     // increment izzyClicks
     izzyClicks++;
@@ -17,34 +65,7 @@ izzy.addEventListener("click", function() {
     // by kyledoesdev on github
     localStorage.setItem('izzy_clicks', izzyClicks);
     
-    // grab random emotion
-    var randomEmotion = izzyEmotions[Math.floor(Math.random() * izzyEmotions.length)];
-    
     //// izzy decision tree
     // popups each click
-    if(izzyClicks == 1) {
-        alert("izzy seems rather puzzled.\n\n izzy: " + String(randomEmotion) + "\n\nyou have clicked izzy " + String(izzyClicks) + " time.")
-    } else if(izzyClicks > 1 && izzyClicks < 5) {
-        alert("maybe she's getting used to you?\n\n izzy: " + String(randomEmotion) + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks >= 5 && izzyClicks < 10) {
-        alert("she starts gnawing on you...\n\n izzy: " + String("^w^") + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks >= 10 && izzyClicks < 20) {
-        alert("izzy is bored of this. maybe you should give up?\n\n izzy: " + String(randomEmotion) + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks == 50) {
-        alert("dude, you're gonna squish her...\n\n izzy: " + String(randomEmotion) + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks >= 60 && izzyClicks <= 75) {
-        alert("oh god... her wings won't work anymore...\n\n izzy: " + String(randomEmotion) + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks >= 90 && izzyClicks <= 99) {
-        alert("STOP!!!!!!!!!!!!!!!!!!!!!!\n\n izzy: " + String(randomEmotion) + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks >= 100 && izzyClicks <= 499) {
-        alert("izzy has died. you moron. you f#cking killed her.\n\n izzy: " + String("X_X") + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks >= 500 && izzyClicks <= 999) {
-        alert("get a life, scumbag.\n\n izzy: " + String("X_X") + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks >= 1000 && izzyClicks <= 4999) {
-        alert("RECONSIDER EXISTING!!!!!!! F#CK!!!.\n\n izzy: " + String("X_X") + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else if(izzyClicks >= 5000) {
-        alert("okay. you revived her. happy?\n\n izzy: " + String(randomEmotion) + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    } else {
-        alert("stop clicking her! wtf!\n\n izzy: " + String(randomEmotion) + "\n\nyou have clicked izzy " + String(izzyClicks) + " times.")
-    }
+    outputIzzyMessage(izzyClicks);
 })
